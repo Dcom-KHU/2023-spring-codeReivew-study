ecb44867a697cb67439a84d01b07ae2ae064f976
rohsik2 <rohsik2@gmail.com>
2023-03-12 12:38:46 +0900

    feat(Application) : 자동차 이름을 받는 생성자 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,23 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+
+	private static final String CAR_NAME_DELIMITER = ",";
+	private final List<Car> cars;
+
+	public Application(String carNames) {
+		cars = new ArrayList<>();
+		String[] carNamesArray = carNames.split(CAR_NAME_DELIMITER);
+		for (String carName : carNamesArray) {
+			cars.add(new Car(carName));
+		}
+	}
+
+	public static void main(String[] args) {
+	}
 }
