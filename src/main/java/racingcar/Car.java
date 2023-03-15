32e13ebed6ddc39e51a9bc01ea8609bcc815eeac
Untaini <hgsu0806@khu.ac.kr>
2023-03-15 14:23:59 +0900

    feat: move and print function in Car class
    Add move function:
     - pick one value in range [0, 9]
     - move +1 position if value >= 4
    
    Add toString function
     - return car name and moving position string
    
    Add getPosition function
     - get position value of car instance

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,11 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -9,4 +15,26 @@ public class Car {
     }
 
     // 추가 기능 구현
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public String toString() {
+        return String.format("%s : %s",this.name, getMoveString());
+    }
+
+    public void move() {
+        if (pickNumberInRange(0, 9) >= 4) {
+            this.position += 1;
+        }
+    }
+
+    private String getMoveString() {
+        List<String> movePositionList = Arrays.stream(new String[this.position])
+                .map(s -> "-")
+                .collect(Collectors.toList());
+
+        return String.join("", movePositionList);
+    }
 }
