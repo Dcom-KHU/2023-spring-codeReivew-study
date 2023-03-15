24ca6b515379e3f0aca86377de19bfc935e519b0
rohsik2 <rohsik2@gmail.com>
2023-03-15 14:43:32 +0900

    feat(Application) : 게임에 몇번째 턴인지 체크하고, 입력받는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,10 +10,14 @@ public class Application {
 
     private static final String CAR_NAME_DELIMITER = ",";
     private final List<Car> cars;
+    private final int maxTurnCount;
 
-    public Application(String[] carNames) {
-        cars = new ArrayList<>();
+    public Application(String[] carNames, int maxTurnCount) {
         checkDuplicateName(carNames);
+        checkTurnValid(maxTurnCount);
+
+        cars = new ArrayList<>();
+        this.maxTurnCount = maxTurnCount;
         for (String carName : carNames) {
             cars.add(new Car(carName));
         }
@@ -23,6 +27,12 @@ public class Application {
         Set<String> carNamesSet = new HashSet<>(Arrays.asList(carNamesArray));
         if (carNamesSet.size() != carNamesArray.length) {
             throw new IllegalArgumentException("[Error] 중복된 이름이 있습니다.");
+        }
+    }
+
+    private void checkTurnValid(int turn) {
+        if (turn < 1) {
+            throw new IllegalArgumentException("[Error] 1 이상의 숫자를 입력해주세요.");
         }
     }
 
