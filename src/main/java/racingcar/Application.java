78a914ed8d692e3a2923b9640245fdde491eb5a2
Jaehooni <8362532@gmail.com>
2023-03-16 02:09:54 +0900

    feat: make execute code

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,12 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
-        Arraylist<Car> cars = Car.make();
+        ArrayList<Car> cars = Car.make();
         Car.game(cars);
+        Car.printResult(cars);
     }
 }
