729860fb17851a53629ed4b1cf7e36c25e4141a0
Yuseunggeun <yuu3730@naver.com>
2023-03-15 14:15:50 +0900

    feat: generate random number and control position

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,16 +1,25 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class Car {
     private final String name;
     private int position = 0;
-
     public Car(String name) {
         this.name = name;
     }
-
     public String getName(){
         return this.name;
     }
 
-    // 추가 기능 구현
+    public void updatePosition(){
+
+        if(canAdvance()){
+            position++;
+        }
+    }
+    public boolean canAdvance(){
+
+        return pickNumberInRange(0, 9) >= 4;
+    }
+
 }
