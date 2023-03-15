7a8542e9fb2164e31353ce802d44b90fac8a5329
Jaehooni <8362532@gmail.com>
2023-03-16 03:00:25 +0900

    refactor: make project convention style same as naver java convention

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,10 +3,9 @@ package racingcar;
 import java.util.ArrayList;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-        ArrayList<Car> cars = Car.make();
-        Car.game(cars);
-        Car.printResult(cars);
-    }
+	public static void main(String[] args) {
+		ArrayList<Car> cars = Car.make();
+		Car.game(cars);
+		Car.printResult(cars);
+	}
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,63 +1,66 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
-    private final String name;
-    private int position = 0;
+	private final String name;
+	private int position = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    public int getPosition() { return this.position; }
+	public int getPosition() {
+		return this.position;
+	}
 
-    // 추가 기능 구현
-    public void move(){
-        if (Randoms.pickNumberInRange(0, 9) >= 4){
-            this.position += 1;
-        }
-    }
-    public static void game(ArrayList<Car> cars){
-        int num = InputCheck.inputRound();
-        for (int i = 0; i < num; i++){
-            System.out.println("실행 결과");
-            for (Car car : cars){
-                car.move();
-                car.printProgress();
-            }
-            System.out.println();
-        }
-    }
-    public static ArrayList<Car> make(){
-        ArrayList<String> names = InputCheck.inputCarNames();
-        ArrayList<Car> Cars = new ArrayList<>();
-        for (String name : names){
-            Cars.add(new Car(name));
-        }
+	// 추가 기능 구현
+	public void move() {
+		if (Randoms.pickNumberInRange(0, 9) >= 4) {
+			this.position += 1;
+		}
+	}
 
-        return Cars;
-    }
+	public static void game(ArrayList<Car> cars) {
+		int num = InputCheck.inputRound();
+		for (int i = 0; i < num; i++) {
+			System.out.println("실행 결과");
+			for (Car car : cars) {
+				car.move();
+				car.printProgress();
+			}
+			System.out.println();
+		}
+	}
 
-    public void printProgress(){
-        String distance = new String(new char[this.position]).replace("\0", "-");
-        System.out.printf("%s : %s%n", this.name, distance);
-    }
+	public static ArrayList<Car> make() {
+		ArrayList<String> names = InputCheck.inputCarNames();
+		ArrayList<Car> cars = new ArrayList<>();
+		for (String name : names) {
+			cars.add(new Car(name));
+		}
+		return cars;
+	}
 
-    public static void printResult(ArrayList<Car> cars){
-        String winner = "";
-        int winnerscore = -1;
+	public void printProgress() {
+		String distance = new String(new char[this.position]).replace("\0", "-");
+		System.out.printf("%s : %s%n", this.name, distance);
+	}
 
-        cars.sort((x, y) -> y.getPosition() - x.getPosition());
-        for (Car car : cars){
-            if (car.getPosition() >= winnerscore) {
-                winner += car.name + ", ";
-                winnerscore = car.position;
-            }
-        }
+	public static void printResult(ArrayList<Car> cars) {
+		String winner = "";
+		int winnerScore = -1;
 
-        System.out.printf("최종 우승자 : %s", winner.substring(0, winner.length() - 2));
-    }
+		cars.sort((x, y) -> y.getPosition() - x.getPosition());
+		for (Car car : cars) {
+			if (car.getPosition() >= winnerScore) {
+				winner += car.name + ", ";
+				winnerScore = car.position;
+			}
+		}
+
+		System.out.printf("최종 우승자 : %s", winner.substring(0, winner.length() - 2));
+	}
 }
