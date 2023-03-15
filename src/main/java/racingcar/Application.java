cd48ee3717f539dabba58c32267e8044a54e3d40
Untaini <hgsu0806@khu.ac.kr>
2023-03-15 16:28:49 +0900

    feat: playAllRounds function
    Add new function
     - repeat rounds as many times as user input
     - each round, all cars move and prints out their own status

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,29 @@ public class Application {
     public static void main(String[] args) {
         List<Car> carList = InputUtils.getInstance().getCarListFromInput();
         int totalRound = InputUtils.getInstance().getTotalRoundFromInput();
+
+        playAllRounds(totalRound, carList);
+    }
+
+    private static void playAllRounds(int totalRound, List<Car> carList) {
+        System.out.println("\n실행 결과");
+        while (--totalRound >= 0) {
+            playRound(carList);
+        }
+    }
+
+    private static void playRound(List<Car> carList) {
+        moveAllCars(carList);
+        printAllCarsStatus(carList);
+    }
+
+    private static void moveAllCars(List<Car> carList) {
+        carList.stream().forEach(Car::move);
+    }
+
+    private static void printAllCarsStatus(List<Car> carList) {
+        carList.stream().forEach(System.out::println);
+        System.out.println();
     }
 
 
