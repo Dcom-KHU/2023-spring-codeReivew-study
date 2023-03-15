06b3c8b94c73cccbda34a93cd1e76ecf3ee094e1
Untaini <hgsu0806@khu.ac.kr>
2023-03-15 17:03:20 +0900

    feat: printWinners function
    Add new function:
     - find car objects that have the biggest position value
     - print selected car objects' name

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,6 +10,8 @@ public class Application {
         int totalRound = InputUtils.getInstance().getTotalRoundFromInput();
 
         playAllRounds(totalRound, carList);
+
+        printWinners(carList);
     }
 
     private static void playAllRounds(int totalRound, List<Car> carList) {
@@ -23,13 +27,41 @@ public class Application {
     }
 
     private static void moveAllCars(List<Car> carList) {
-        carList.stream().forEach(Car::move);
+        carList.forEach(Car::move);
     }
 
     private static void printAllCarsStatus(List<Car> carList) {
-        carList.stream().forEach(System.out::println);
+        carList.forEach(System.out::println);
         System.out.println();
     }
+
+    private static void printWinners(List<Car> carList) {
+        System.out.printf("최종 우승자 : %s", getWinnersString(carList));
+    }
+
+    private static String getWinnersString(List<Car> carList) {
+        List<String> winnerNameList = findWinners(carList).stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+
+        return String.join(", ", winnerNameList);
+    }
+
+    private static List<Car> findWinners(List<Car> carList) {
+        if (carList.size() == 0) {
+            return Collections.emptyList();
+        }
+
+        int maxPosition = carList.stream()
+                .map(Car::getPosition)
+                .max(Integer::compareTo)
+                .get();
+
+        return carList.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
+    }
+
 
 
 }
