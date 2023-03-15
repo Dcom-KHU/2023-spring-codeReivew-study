7b5be118e86a9cf77c59aa45b5f348393f8f44e3
Choi Seongwon <vlflalektzja@naver.com>
2023-03-16 00:31:13 +0900

    feat: get Car names

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        List<Car> cars = getCars();
+    }
+
+    public static List<Car> getCars() {
+        String carName = Console.readLine();
+        String[] carNames = carName.split(",");
+
+        List<Car> Cars = new ArrayList<>();
+        for (String name : carNames) {
+            Car car = new Car(name);
+            Cars.add(car);
+        }
+        return Cars;
     }
 }
