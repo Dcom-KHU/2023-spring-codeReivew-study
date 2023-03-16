48f3e95c923e101b32b036daab649bcaa0edb375
Choi Seongwon <vlflalektzja@naver.com>
2023-03-16 23:32:56 +0900

    feat: create random number

--- src/main/java/racingcar/CarGame.java
+++ src/main/java/racingcar/CarGame.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import view.InputView;
 
 import java.util.List;
@@ -14,5 +15,18 @@ public class CarGame {
     public void start() {
         List<Car> cars = inputView.getCars();
         Integer trial = inputView.getTrial();
+        startGame(cars, trial);
+    }
+
+    private void startGame(List<Car> cars, Integer trial) {
+        for (int i = 0; i < trial; i++) {
+            for (Car car : cars) {
+                go(car);
+            }
+        }
+    }
+
+    private void go(Car car) {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
     }
 }
