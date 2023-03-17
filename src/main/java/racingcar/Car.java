57ecb6ab934ff9e5d4539bb3f3f2bd43db4d7026
lomayd <jinho10167@gmail.com>
2023-03-17 10:09:29 +0900

    feat: get Car list

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,10 @@
 package racingcar;
 
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -8,5 +13,14 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public static List<Car> getCarList(String input) {
+        List<Car> carList = new ArrayList<>();
+        List<String> inputList = new ArrayList(Arrays.asList(input.split(",")));
+
+        for(String s : inputList) {
+            Car car = new Car(s);
+            carList.add(car);
+        }
+        return carList;
+    }
 }
