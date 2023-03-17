d0ba0705155696b7e776d48a3ebdefbdfded12ae
lomayd <jinho10167@gmail.com>
2023-03-17 14:52:58 +0900

    feat: apply Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        String input = Console.readLine();
+        List<Car> carList = racingcar.Car.getCarList(input);
+
+        Integer count = racingcar.Car.getGameCount(Console.readLine());
+
+        racingcar.Car.playGame(carList, count);
+
+        racingcar.Car.getWinner(carList);
     }
 }
