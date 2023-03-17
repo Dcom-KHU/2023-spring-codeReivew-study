3cc2b3db5da133e75df15ca55baf33ce06d556ff
Nahyun Kim <knh4769@khu.ac.kr>
2023-03-18 01:22:30 +0900

    feat: 잘못된 값 입력 시 그부분부터 재입력하는 기능 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import racingcar.controller.util.ExceptionHandler;
 import racingcar.dto.CarStatusDTO;
 import racingcar.model.RacingGame;
 import racingcar.model.RandomNumberGenerator;
@@ -14,24 +15,28 @@ public class RacingController {
     private final RacingGame racingGame = new RacingGame(new RandomNumberGenerator());
 
     public void run() {
-        enrollCarToRace();
-        moveCarsByCount();
-        showWinners();
+        executeFunction(this::enrollCarToRace);
+        executeFunction(this::moveCarsByCount);
+        executeFunction(this::showWinners);
     }
 
-    public void enrollCarToRace() {
+    private void executeFunction(Runnable runnable) {
+        ExceptionHandler.retryForIllegalArgument(runnable, outputView::printErrorMessage);
+    }
+
+    private void enrollCarToRace() {
         List<String> carNames = inputView.inputCarNames();
         racingGame.enrollCars(carNames);
     }
 
-    public void moveCarsByCount() {
+    private void moveCarsByCount() {
         int moveCount = inputView.inputMoveCount();
         // moveCount만큼 반복 이동
         List<CarStatusDTO> carStatuses = racingGame.repeatMovingCars(moveCount);
         outputView.printGameResult(carStatuses);
     }
 
-    public void showWinners() {
+    private void showWinners() {
         List<String> winnerNames = racingGame.findWinners();
         outputView.printWinners(winnerNames);
     }
