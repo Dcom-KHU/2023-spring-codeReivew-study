975675b2038311864724f8d87b6ec13b0b529373
sschan99 <seungchan1219@gmail.com>
2023-03-18 20:13:40 +0900

    feat(Car): name의 getter 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -10,6 +10,10 @@ public class Car {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public int getPosition() {
         return position;
     }
