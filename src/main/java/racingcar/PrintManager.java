b1187829afc66db27d2e49b8789510f43415beac
Untaini <hgsu0806@khu.ac.kr>
2023-03-19 00:21:15 +0900

    refactor: add PrintManager class
    add PrintManager class for printing
    
    collect print func from other classes
     - printCarNameInputDescription (in InputUtils)
     - printTotalRoundInputDescription (in InputUtils)
     - printErrorMessage (in InputUtils)
    
     - printAllCarsStatus (in Application)
     - printWinnersName (in Application)
     - printGameResultHead (in Application)

---
+++ src/main/java/racingcar/PrintManager.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PrintManager {
+
+    public static void printCarNameInputDescription() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printTotalRoundInputDescription() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printAllCarsStatus(List<Car> carList) {
+        carList.forEach(System.out::println);
+        System.out.println();
+    }
+
+    public static void printWinnersName(List<Car> carList) {
+        List<String> winnerNameList = carList.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+
+        String winnersName = String.join(", ", winnerNameList);
+
+        System.out.printf("최종 우승자 : %s", winnersName);
+    }
+
+    public static void printGameResultHead() {
+        System.out.println("\n실행 결과");
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.printf("[ERROR] %s\n", message);
+    }
+}
