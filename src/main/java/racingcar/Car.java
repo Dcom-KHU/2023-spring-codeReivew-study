5209627fc0ed2cb7f1f56dfc8b24c7d97eaad847
WoonKyung Kim <splguyjr@naver.com>
2023-03-18 20:51:27 +0900

    feat: Car구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,49 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private final String name;
     private int position = 0;
-
+    private int distance = 0;
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public int GenerateRandomNumber() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean CheckAdvance(int num) {
+        if(num>=4) {
+            return true; }
+        return false;
+    }
+
+    public void Advance() {
+        this.distance++;
+    }
+
+    public void show_result() {
+        System.out.print(this.name + " : ");
+        for(int i=0; i<this.distance; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public void show_winner() {
+        System.out.print("최종 우승자 : " + this.name);
+
+    }
+
+
 }
