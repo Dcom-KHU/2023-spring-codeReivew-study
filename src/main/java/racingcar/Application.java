8dc465df7219cd7be487cfceba48f9858246ab77
Untaini <hgsu0806@khu.ac.kr>
2023-03-19 01:46:03 +0900

    refactor: use Game instance and remove unused function
    use Game instance instead of static function call
    remove unused function in Application
    
    removed function:
     - playAllRounds
     - findWinners
     - getWinnersString
    
     - playRound
     - moveAppCars
    
     - printAllCarsStatus
     - printWinners

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,64 +1,13 @@
 package racingcar;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        List<Car> carList = InputUtils.getInstance().getCarListFromInput();
-        int totalRound = InputUtils.getInstance().getTotalRoundFromInput();
+        List<Car> carList = InputUtils.getCarListFromInput();
+        int totalRound = InputUtils.getTotalRoundFromInput();
+        Game game = new Game(carList, totalRound);
 
-        playAllRounds(totalRound, carList);
-
-        printWinners(carList);
-    }
-
-    private static void playAllRounds(int totalRound, List<Car> carList) {
-        System.out.println("\n실행 결과");
-        while (--totalRound >= 0) {
-            playRound(carList);
-        }
-    }
-
-    private static void playRound(List<Car> carList) {
-        moveAllCars(carList);
-        printAllCarsStatus(carList);
-    }
-
-    private static void moveAllCars(List<Car> carList) {
-        carList.forEach(Car::move);
-    }
-
-    private static void printAllCarsStatus(List<Car> carList) {
-        carList.forEach(System.out::println);
-        System.out.println();
-    }
-
-    private static void printWinners(List<Car> carList) {
-        System.out.printf("최종 우승자 : %s", getWinnersString(carList));
-    }
-
-    private static String getWinnersString(List<Car> carList) {
-        List<String> winnerNameList = findWinners(carList).stream()
-                .map(Car::getName)
-                .collect(Collectors.toList());
-
-        return String.join(", ", winnerNameList);
-    }
-
-    private static List<Car> findWinners(List<Car> carList) {
-        if (carList.size() == 0) {
-            return Collections.emptyList();
-        }
-
-        int maxPosition = carList.stream()
-                .map(Car::getPosition)
-                .max(Integer::compareTo)
-                .get();
-
-        return carList.stream()
-                .filter(car -> car.getPosition() == maxPosition)
-                .collect(Collectors.toList());
+        game.start();
     }
 }
