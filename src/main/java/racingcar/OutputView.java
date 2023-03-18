b379fe38dd24af9c7025981f42508f1a4b3ffdde
sschan99 <seungchan1219@gmail.com>
2023-03-18 20:17:38 +0900

    feat(OutputView): 최종 우승자 목록을 구하고 출력하는 기능 구현

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
@@ -13,5 +14,20 @@ public class OutputView {
     public void printPreface() {
         System.out.println();
         System.out.println("실행 결과");
+    }
+
+    public void printFinalWinner(List<Car> cars) {
+        List<String> finalWinner = new ArrayList<>();
+        int max = 0;
+        for (Car car : cars) {
+            if (car.getPosition() > max) {
+                finalWinner.clear();
+                max = car.getPosition();
+            }
+            if (car.getPosition() == max) {
+                finalWinner.add(car.getName());
+            }
+        }
+        System.out.println("최종 우승자 : " + String.join(", ", finalWinner));
     }
 }
