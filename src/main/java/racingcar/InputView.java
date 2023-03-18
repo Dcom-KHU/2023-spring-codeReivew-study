928b6a199a4aa1b1ba5f139e76ef4215dc4817fe
sschan99 <seungchan1219@gmail.com>
2023-03-18 18:54:47 +0900

    feat(InputView): 자동차 이름의 입력이 잘못된 경우 에러 메시지를 출력 후 다시 입력받음

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -7,7 +7,24 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     public List<String> receiveCarNames() {
-        List<String> carNames = Arrays.asList(readLine().split(","));
-        return carNames;
+        while (true) {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            List<String> carNames = Arrays.asList(readLine().split(","));
+            try {
+                validate(carNames);
+            } catch (IllegalArgumentException iae) {
+                System.out.println(iae.getMessage());
+                continue;
+            }
+            return carNames;
+        }
+    }
+
+    private void validate(List<String> carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하만 가능합니다.");
+            }
+        }
     }
 }
