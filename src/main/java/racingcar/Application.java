7b491be167cb9ec37ba5996ec8b63d4f82d3e440
sschan99 <seungchan1219@gmail.com>
2023-03-18 19:58:23 +0900

    feat(Application): 구현한 기능들을 사용하여 자동차 경주 게임 구현 (최종 우승자 안내 제외)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,34 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        List<Car> cars = initializeCars(inputView.receiveCarNames());
+        int attemptNumber = inputView.receiveAttemptNumber();
+
+        outputView.printPreface();
+        for (int i = 0; i < attemptNumber; i++) {
+            moveCars(cars);
+            outputView.printExecutionResults(cars);
+        }
+    }
+
+    private static List<Car> initializeCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+    private static void moveCars(List<Car> cars) {
+        for (Car car : cars) {
+            car.forward();
+        }
     }
 }
