6e6c23babdcba5381f2a4d48f97ae118be87b6e7
Jaehooni <8362532@gmail.com>
2023-03-21 23:19:47 +0900

    refactor: just fix some codes

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 public class Application {
 	public static void main(String[] args) {
-		List<Car> cars = Car.make();
-		Car.game(cars);
-		Car.printResult(cars);
+		List<Car> cars = Game.make();
+		Game.start(cars);
 	}
 }
--- src/main/java/racingcar/InputCheck.java
+++ src/main/java/racingcar/InputCheck.java
@@ -14,8 +14,7 @@ public final class InputCheck {
 			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
 			List<String> names = new ArrayList<>(Arrays.asList(Console.readLine().split(",")));
 			try {
-				iterateCars(names);
-				// names.forEach(InputCheck::stringCheck);
+				names.forEach(InputCheck::nameCheck);
 				return names;
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
@@ -23,18 +22,9 @@ public final class InputCheck {
 		}
 	}
 
-	// public static void stringCheck(String name) {
-	// 	if (name.length() == 0 || name.length() > 5) {
-	// 		throw new IllegalArgumentException("[ERROR] 이름은 1~5자 사이로 작성하라.");
-	// 	}
-	// }
-
-	public static void iterateCars(List<String> names) {
-
-		for (String name : names) {
-			if (name.length() > 5) {
-				throw new IllegalArgumentException();
-			}
+	private static void nameCheck(String name) {
+		if (name.length() == 0 || name.length() > 5) {
+			throw new IllegalArgumentException("[ERROR] 이름은 1~5자 사이로 작성하라.");
 		}
 	}
 
@@ -43,18 +33,18 @@ public final class InputCheck {
 			System.out.println("시도할 회수는 몇회인가요?");
 			try {
 				String input = Console.readLine();
-				match(input);
+				roundCheck(input);
 				return Integer.parseInt(input);
 			} catch (IllegalArgumentException e) {
-				System.out.println("[ERROR] 시도 횟수는 자연수인 숫자여야 한다.");
+				System.out.println(e.getMessage());
 			}
 		}
 	}
 
-	public static void match(String input) {
+	private static void roundCheck(String input) {
 		String pattern = "^[0-9]+$";
 		if (!Pattern.matches(pattern, input)) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("[ERROR] 시도 횟수는 자연수인 숫자여야 한다.");
 		}
 	}
 }
