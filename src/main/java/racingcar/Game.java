bfa1c4eeb198bba9bf213504f019f295d827b25b
Jaehooni <8362532@gmail.com>
2023-03-24 15:18:23 +0900

    style: change parameter name
    - carList -> car

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-	private List<Car> carList = new ArrayList<>();
+	private List<Car> cars = new ArrayList<>();
 
 	public void make() {
 		List<String> names = InputCheck.inputCarNames();
 		for (String name : names) {
-			this.carList.add(new Car(name));
+			this.cars.add(new Car(name));
 		}
 	}
 
 	private void run() {
 		int num = InputCheck.inputRound();
 		for (int i = 0; i < num; i++) {
-			this.carList.forEach(Car::move);
-			Output.printProcedure(this.carList);
+			this.cars.forEach(Car::move);
+			Output.printProcedure(this.cars);
 		}
 	}
 
 	public void start() {
 		run();
-		Output.printResult(this.carList);
+		Output.printResult(this.cars);
 	}
 }
