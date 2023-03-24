d90073c947021c4c090ab279707253a26255ba7a
Jaehooni <8362532@gmail.com>
2023-03-24 15:20:34 +0900

    refactor: separate sort function from findMaxValue(...)
    new function
    - sortByPosition

--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Output {
-	public static void printProcedure(List<Car> carList) {
+	public static void printProcedure(List<Car> cars) {
 		System.out.println("실행 결과");
-		for (Car car : carList) {
+		for (Car car : cars) {
 			String distance = new String(new char[car.getPosition()]).replace("\0", "-");
 			System.out.printf("%s : %s%n", car.getName(), distance);
 		}
 		System.out.println();
 	}
 
-	static void printResult(List<Car> carList) {
+	static void printResult(List<Car> cars) {
 		List<String> winner = new ArrayList<>();
-		int maxValue = findMaxValue(carList);
+		int maxValue = findMaxValue(cars);
 
-		for (Car car : carList) {
+		for (Car car : cars) {
 			if (car.getPosition() < maxValue) {
 				break;
 			}
@@ -27,8 +27,12 @@ public class Output {
 		System.out.printf("최종 우승자 : %s", String.join(", ", winner));
 	}
 
-	private static int findMaxValue(List<Car> carList) {
-		carList.sort((x, y) -> y.getPosition() - x.getPosition());
-		return carList.get(0).getPosition();
+	private static int findMaxValue(List<Car> cars) {
+		sortByPosition(cars);
+		return cars.get(0).getPosition();
+	}
+
+	private static void sortByPosition(List<Car> cars) {
+		cars.sort((x, y) -> y.getPosition() - x.getPosition());
 	}
 }
