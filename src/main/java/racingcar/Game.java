582d57f25b5e00d8d98f256ca958007a1ca559d8
Jaehooni <8362532@gmail.com>
2023-03-25 15:08:48 +0900

    refactor: move sort func to Sort.java

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -3,10 +3,14 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 
+import utility.InputCheck;
+import utility.Output;
+import utility.Sort;
+
 public class Game {
 	private List<Car> cars = new ArrayList<>();
 
-	public void make() {
+	private void make() {
 		List<String> names = InputCheck.inputCarNames();
 		for (String name : names) {
 			this.cars.add(new Car(name));
@@ -21,9 +25,22 @@ public class Game {
 		}
 	}
 
+	public List<String> findWinners() {
+		List<String> winner = new ArrayList<>();
+		List<Car> sortedCars = Sort.sortByPosition(this.cars);
+		int winnerScore = sortedCars.get(0).getPosition();
+		for (Car car : sortedCars) {
+			if (car.getPosition() < winnerScore) {
+				break;
+			}
+			winner.add(car.getName());
+		}
+		return winner;
+	}
+
 	public void start() {
 		make();
 		run();
-		Output.printResult(this.cars);
+		Output.printResult(findWinners());
 	}
 }
