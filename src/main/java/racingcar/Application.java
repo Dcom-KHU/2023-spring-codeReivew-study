c37f2f8d41cf21682140337f6dd9036860c2e6a7
WoonKyung Kim <splguyjr@naver.com>
2023-03-25 10:53:42 +0900

    feat: 구조 변경 및 메소드화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,89 +1,76 @@
 package racingcar;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
+
 public class Application {
+
+    private List<Car> cars;
+    private InputManager inputManager;
+    private OutputManger outputManger;
+    private int totalAttemptNum;
+
+
     public static void main(String[] args) {
-        //getInput();
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Scanner scanner = new Scanner(System.in);
+        Application app = new Application();
+        app.inputManager = new InputManager();
+        app.outputManger = new OutputManger();
+        app.run();
+    }
 
-        String input = Console.readLine();
+    public void run() {
+        getInfo();
+        simulation();
+        finishGame();
+    }
 
-        String[] car_name = input.split(",");
-        Car[] cars = new Car[car_name.length];
+    private void getInfo() {
+        cars = new ArrayList<>();
 
+        outputManger.printGetCarName();
+        String[] carName = inputManager.getCar();
 
+        for(String name : carName) {
+            cars.add(new Car(name));
+        }
 
-        int attempt =0;
-        boolean validInput = false;
+        outputManger.printGetAttemptNum();
+        this.totalAttemptNum = inputManager.getAttemptNum();
 
-        //
-        while(!validInput) {
-            try {
-                System.out.println("시도할 회수는 몇회인가요?");
-                attempt = scanner.nextInt();
-                validInput = true;
-            } catch (InputMismatchException e) {
-                System.out.println("[ERROR] 시도 횟수는 숫자여야 한다.");
-                Console.readLine();
+        outputManger.printRunResultMessage();
+    }
+
+    private void simulation() {
+
+       for(int i=0; i<totalAttemptNum; i++) {
+           for (Car car : cars) {
+               car.CheckAdvance(car.GenerateRandomNumber());
+           }
+           outputManger.printSimulationResult(cars);
+       }
+    }
+
+    private List<Car> calWinner() {
+        int maxPosition = 0;
+        List<Car> winners = new ArrayList<Car>();
+
+        for (Car car : cars) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car);
             }
         }
 
-        System.out.println("\n실행 결과");
+        return winners;
+    }
 
-        //makeCarInstance
-        for(int i=0; i< car_name.length; i++) {
-            String carName = car_name[i];
-            cars[i] = new Car(carName);
-        }
-
-        int[] distance_arr = new int[car_name.length];
-
-        for(int j=0; j<attempt; j++) {
-            for(int i=0; i< car_name.length; i++) {
-                int check = cars[i].GenerateRandomNumber();
-                if (cars[i].CheckAdvance(check)) {
-                    cars[i].Advance();
-                }
-                cars[i].show_result();
-                if(j == attempt-1) {
-                    distance_arr[i] = cars[i].getDistance();
-                }
-            }
-            System.out.println();
-        }
-
-        //checkMax
-        int max = 0;
-
-        for(int i=0; i< car_name.length; i++) {
-            if(distance_arr[i]> max) {
-                max = distance_arr[i];
-            }
-        }
-        int cnt = 0;
-        for(int i=0; i< car_name.length; i++) {
-            if(distance_arr[i] == max) {
-                cnt++;
-            }
-        }
-
-        //printWinner carArray
-        System.out.print("최초 우승자 : ");
-        for(int i=0; i< car_name.length; i++) {
-                if (distance_arr[i] == max) {
-                    if (cnt > 1) {
-                        System.out.print(cars[i].getName() + ", ");
-                        cnt--;
-                    }
-                    else {
-                        System.out.println(cars[i].getName());
-                    }
-                }
-        }
+    private void finishGame() {
+        outputManger.printWinner(calWinner());
     }
 
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,9 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Car {
     private final String name;
     private int position = 0;
-    private int distance = 0;
     public Car(String name) {
         this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public int GenerateRandomNumber() {
@@ -14,36 +21,13 @@ public class Car {
         return randomNumber;
     }
 
-    public int getDistance() {
-        return distance;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public boolean CheckAdvance(int num) {
-        if(num>=4) {
-            return true; }
-        return false;
-    }
-
-    public void Advance() {
-        this.distance++;
-    }
-
-    public void show_result() {
-        System.out.print(this.name + " : ");
-        for(int i=0; i<this.distance; i++) {
-            System.out.print("-");
+    public void CheckAdvance(int num) {
+        if(this.GenerateRandomNumber()>=4) {
+            this.position++;
         }
-        System.out.println();
     }
 
-    public void show_winner() {
-        System.out.print("최종 우승자 : " + this.name);
 
-    }
 
 
 }
